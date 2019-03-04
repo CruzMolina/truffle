4dc9459bd7d8822c70c3606ed2b705870deaa6da
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-03-04 09:48:04 -0800

    modernize commands/test.js

--- packages/truffle-core/lib/commands/test.js
+++ packages/truffle-core/lib/commands/test.js
@@ -1,4 +1,4 @@
-var command = {
+const command = {
   command: "test",
   description: "Run JavaScript and Solidity tests",
   builder: {
@@ -41,19 +41,19 @@ var command = {
       }
     ]
   },
-  run: function(options, done) {
-    var OS = require("os");
-    var dir = require("node-dir");
-    var temp = require("temp");
-    var Config = require("truffle-config");
-    var Artifactor = require("truffle-artifactor");
-    var Develop = require("../develop");
-    var Test = require("../test");
-    var fs = require("fs");
-    var copy = require("../copy");
-    var Environment = require("../environment");
+  run(options, done) {
+    const OS = require("os");
+    const dir = require("node-dir");
+    const temp = require("temp");
+    const Config = require("truffle-config");
+    const Artifactor = require("truffle-artifactor");
+    const Develop = require("../develop");
+    const Test = require("../test");
+    const fs = require("fs");
+    const copy = require("../copy");
+    const Environment = require("../environment");
 
-    var config = Config.detect(options);
+    const config = Config.detect(options);
 
     // if "development" exists, default to using that for testing
     if (!config.network && config.networks.development) {
@@ -64,9 +64,9 @@ var command = {
       config.network = "test";
     }
 
-    var ipcDisconnect;
+    let ipcDisconnect;
 
-    var files = [];
+    let files = [];
 
     if (options.file) {
       files = [options.file];
@@ -82,20 +82,20 @@ var command = {
       dir.files(config.test_directory, callback);
     }
 
-    getFiles(function(err, files) {
+    getFiles((err, files) => {
       if (err) return done(err);
 
-      files = files.filter(function(file) {
-        return file.match(config.test_file_extension_regexp) != null;
-      });
+      files = files.filter(
+        file => file.match(config.test_file_extension_regexp) != null
+      );
 
-      temp.mkdir("test-", function(err, temporaryDirectory) {
+      temp.mkdir("test-", (err, temporaryDirectory) => {
         if (err) return done(err);
 
         function cleanup() {
-          var args = arguments;
+          const args = arguments;
           // Ensure directory cleanup.
-          temp.cleanup(function() {
+          temp.cleanup(() => {
             // Ignore cleanup errors.
             done.apply(null, args);
             if (ipcDisconnect) {
@@ -118,22 +118,18 @@ var command = {
           );
         }
 
-        var environmentCallback = function(err) {
+        const environmentCallback = err => {
           if (err) return done(err);
           // Copy all the built files over to a temporary directory, because we
           // don't want to save any tests artifacts. Only do this if the build directory
           // exists.
-          fs.stat(config.contracts_build_directory, function(err) {
+          fs.stat(config.contracts_build_directory, err => {
             if (err) return run();
 
-            copy(config.contracts_build_directory, temporaryDirectory, function(
-              err
-            ) {
+            copy(config.contracts_build_directory, temporaryDirectory, err => {
               if (err) return done(err);
 
-              config.logger.log(
-                "Using network '" + config.network + "'." + OS.EOL
-              );
+              config.logger.log(`Using network '${config.network}'.${OS.EOL}`);
 
               run();
             });
@@ -143,11 +139,11 @@ var command = {
         if (config.networks[config.network]) {
           Environment.detect(config, environmentCallback);
         } else {
-          var ipcOptions = {
+          const ipcOptions = {
             network: "test"
           };
 
-          var ganacheOptions = {
+          const ganacheOptions = {
             host: "127.0.0.1",
             port: 7545,
             network_id: 4447,
@@ -157,13 +153,14 @@ var command = {
             noVMErrorsOnRPCResponse: true
           };
 
-          Develop.connectOrStart(ipcOptions, ganacheOptions, function(
-            started,
-            disconnect
-          ) {
-            ipcDisconnect = disconnect;
-            Environment.develop(config, ganacheOptions, environmentCallback);
-          });
+          Develop.connectOrStart(
+            ipcOptions,
+            ganacheOptions,
+            (started, disconnect) => {
+              ipcDisconnect = disconnect;
+              Environment.develop(config, ganacheOptions, environmentCallback);
+            }
+          );
         }
       });
     });
