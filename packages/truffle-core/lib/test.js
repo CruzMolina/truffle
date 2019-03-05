566b674732c8516362ddb94d9f65205b9a297a95
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-03-05 09:22:20 -0800

    modernize lib/test.js

--- packages/truffle-core/lib/test.js
+++ packages/truffle-core/lib/test.js
@@ -1,24 +1,24 @@
-var Mocha = require("mocha");
-var chai = require("chai");
-var path = require("path");
-var Web3 = require("web3");
-var Config = require("truffle-config");
-var Contracts = require("truffle-workflow-compile");
-var Resolver = require("truffle-resolver");
-var TestRunner = require('./testing/testrunner');
-var TestResolver = require("./testing/testresolver");
-var TestSource = require("./testing/testsource");
-var SolidityTest = require("./testing/soliditytest");
-var expect = require("truffle-expect");
-var Migrate = require("truffle-migrate");
-var Profiler = require("truffle-compile/profiler.js");
-var originalrequire = require("original-require");
+const Mocha = require("mocha");
+const chai = require("chai");
+const path = require("path");
+const Web3 = require("web3");
+const Config = require("truffle-config");
+const Contracts = require("truffle-workflow-compile");
+const Resolver = require("truffle-resolver");
+const TestRunner = require("./testing/testrunner");
+const TestResolver = require("./testing/testresolver");
+const TestSource = require("./testing/testsource");
+const SolidityTest = require("./testing/soliditytest");
+const expect = require("truffle-expect");
+const Migrate = require("truffle-migrate");
+const Profiler = require("truffle-compile/profiler.js");
+const originalrequire = require("original-require");
 
 chai.use(require("./assertions"));
 
-var Test = {
-  run: function(options, callback) {
-    var self = this;
+const Test = {
+  run: (options, callback) => {
+    const self = this;
 
     expect.options(options, [
       "contracts_directory",
@@ -27,24 +27,24 @@ var Test = {
       "test_files",
       "network",
       "network_id",
-      "provider",
+      "provider"
     ]);
 
-    var config = Config.default().merge(options);
+    const config = Config.default().merge(options);
 
-    config.test_files = config.test_files.map(function(test_file) {
-      return path.resolve(test_file);
-    });
+    config.test_files = config.test_files.map(test_file =>
+      path.resolve(test_file)
+    );
 
     // `accounts` will be populated before each contract() invocation
     // and passed to it so tests don't have to call it themselves.
-    var web3 = new Web3();
+    const web3 = new Web3();
     web3.setProvider(config.provider);
 
     // Override console.warn() because web3 outputs gross errors to it.
     // e.g., https://github.com/ethereum/web3.js/blob/master/lib/web3/allevents.js#L61
     // Output looks like this during tests: https://gist.github.com/tcoulter/1988349d1ec65ce6b958
-    var warn = config.logger.warn;
+    const warn = config.logger.warn;
     config.logger.warn = function(message) {
       if (message === "cannot find event for log") {
         return;
@@ -55,19 +55,19 @@ var Test = {
       }
     };
 
-    var mocha = this.createMocha(config);
+    const mocha = this.createMocha(config);
 
-    var js_tests = config.test_files.filter(function(file) {
-      return path.extname(file) !== ".sol";
-    });
+    const js_tests = config.test_files.filter(
+      file => path.extname(file) !== ".sol"
+    );
 
-    var sol_tests = config.test_files.filter(function(file) {
-      return path.extname(file) === ".sol";
-    });
+    const sol_tests = config.test_files.filter(
+      file => path.extname(file) === ".sol"
+    );
 
     // Add Javascript tests because there's nothing we need to do with them.
     // Solidity tests will be handled later.
-    js_tests.forEach(function(file) {
+    js_tests.forEach(file => {
       // There's an idiosyncracy in Mocha where the same file can't be run twice
       // unless we delete the `require` cache.
       // https://github.com/mochajs/mocha/issues/995
@@ -76,56 +76,69 @@ var Test = {
       mocha.addFile(file);
     });
 
-    var dependency_paths = [];
-    var testContracts = [];
-    var accounts = [];
-    var runner;
-    var test_resolver;
+    let dependency_paths = [];
+    let testContracts = [];
+    let accounts = [];
+    let runner;
+    let test_resolver;
 
-    web3.eth.getAccounts().then(function(accs) {
-      accounts = accs;
+    web3.eth
+      .getAccounts()
+      .then(accs => {
+        accounts = accs;
 
-      if (!config.resolver) {
-        config.resolver = new Resolver(config);
-      }
+        if (!config.resolver) {
+          config.resolver = new Resolver(config);
+        }
 
-      var test_source = new TestSource(config);
-      test_resolver = new TestResolver(config.resolver, test_source, config.contracts_build_directory);
-      test_resolver.cache_on = false;
+        const test_source = new TestSource(config);
+        test_resolver = new TestResolver(
+          config.resolver,
+          test_source,
+          config.contracts_build_directory
+        );
+        test_resolver.cache_on = false;
 
-      return self.compileContractsWithTestFilesIfNeeded(sol_tests, config, test_resolver);
-    }).then(function(paths) {
-      dependency_paths = paths;
+        return self.compileContractsWithTestFilesIfNeeded(
+          sol_tests,
+          config,
+          test_resolver
+        );
+      })
+      .then(paths => {
+        dependency_paths = paths;
 
-      testContracts = sol_tests.map(function(test_file_path) {
-        var built_name = "./" + path.basename(test_file_path);
-        return test_resolver.require(built_name);
-      });
+        testContracts = sol_tests.map(test_file_path => {
+          const built_name = `./${path.basename(test_file_path)}`;
+          return test_resolver.require(built_name);
+        });
 
-      runner = new TestRunner(config);
+        runner = new TestRunner(config);
 
-      return self.performInitialDeploy(config, test_resolver);
-    }).then(function() {
-      return self.defineSolidityTests(mocha, testContracts, dependency_paths, runner);
-    }).then(function() {
-      return self.setJSTestGlobals(web3, accounts, test_resolver, runner);
-    }).then(function() {
-      // Finally, run mocha.
-      process.on('unhandledRejection', function(reason, p) {
-        throw reason;
-      });
+        return self.performInitialDeploy(config, test_resolver);
+      })
+      .then(() =>
+        self.defineSolidityTests(mocha, testContracts, dependency_paths, runner)
+      )
+      .then(() => self.setJSTestGlobals(web3, accounts, test_resolver, runner))
+      .then(() => {
+        // Finally, run mocha.
+        process.on("unhandledRejection", reason => {
+          throw reason;
+        });
 
-      mocha.run(function(failures) {
-        config.logger.warn = warn;
+        mocha.run(failures => {
+          config.logger.warn = warn;
 
-        callback(failures);
-      });
-    }).catch(callback);
+          callback(failures);
+        });
+      })
+      .catch(callback);
   },
 
-  createMocha: function(config) {
+  createMocha: config => {
     // Allow people to specify config.mocha in their config.
-    var mochaConfig = config.mocha || {};
+    const mochaConfig = config.mocha || {};
 
     // If the command line overrides color usage, use that.
     if (config.colors != null) {
@@ -137,71 +150,81 @@ var Test = {
       mochaConfig.useColors = true;
     }
 
-    var mocha = new Mocha(mochaConfig);
+    const mocha = new Mocha(mochaConfig);
 
     return mocha;
   },
 
-  compileContractsWithTestFilesIfNeeded: function(solidity_test_files, config, test_resolver) {
-    return new Promise(function(accept, reject) {
-      Profiler.updated(config.with({
-        resolver: test_resolver
-      }), function(err, updated) {
-        if (err) return reject(err);
-
-        updated = updated || [];
-
-        // Compile project contracts and test contracts
-        Contracts.compile(config.with({
-          all: config.compileAll === true,
-          files: updated.concat(solidity_test_files),
-          resolver: test_resolver,
-          quiet: false,
-          quietWrite: true
-        }), function(err, result) {
+  compileContractsWithTestFilesIfNeeded: (
+    solidity_test_files,
+    config,
+    test_resolver
+  ) =>
+    new Promise((accept, reject) => {
+      Profiler.updated(
+        config.with({
+          resolver: test_resolver
+        }),
+        (err, updated) => {
           if (err) return reject(err);
-          const paths = result.outputs.solc;
-          accept(paths);
-        });
-      });
-    });
-  },
 
-  performInitialDeploy: function(config, resolver) {
-    return new Promise(function(accept, reject) {
-      Migrate.run(config.with({
-        reset: true,
-        resolver: resolver,
-        quiet: true
-      }), function(err) {
-        if (err) return reject(err);
-        accept();
-      });
-    });
-  },
+          updated = updated || [];
 
-  defineSolidityTests: function(mocha, contracts, dependency_paths, runner) {
-    return new Promise(function(accept) {
-      contracts.forEach(function(contract) {
+          // Compile project contracts and test contracts
+          Contracts.compile(
+            config.with({
+              all: config.compileAll === true,
+              files: updated.concat(solidity_test_files),
+              resolver: test_resolver,
+              quiet: false,
+              quietWrite: true
+            }),
+            (err, { outputs }) => {
+              if (err) return reject(err);
+              const paths = outputs.solc;
+              accept(paths);
+            }
+          );
+        }
+      );
+    }),
+
+  performInitialDeploy: (config, resolver) =>
+    new Promise((accept, reject) => {
+      Migrate.run(
+        config.with({
+          reset: true,
+          resolver,
+          quiet: true
+        }),
+        err => {
+          if (err) return reject(err);
+          accept();
+        }
+      );
+    }),
+
+  defineSolidityTests: (mocha, contracts, dependency_paths, runner) =>
+    new Promise(accept => {
+      contracts.forEach(contract => {
         SolidityTest.define(contract, dependency_paths, runner, mocha);
       });
 
       accept();
-    });
-  },
+    }),
 
-  setJSTestGlobals: function(web3, accounts, test_resolver, runner) {
-    return new Promise(function(accept, reject) {
+  setJSTestGlobals: (web3, accounts, test_resolver, runner) =>
+    new Promise(accept => {
       global.web3 = web3;
       global.assert = chai.assert;
       global.expect = chai.expect;
       global.artifacts = {
-        require: function(import_path) {
+        require(import_path) {
           return test_resolver.require(import_path);
         }
       };
 
-      var template = function(tests) {
+      const template = function(tests) {
         this.timeout(runner.TEST_TIMEOUT);
 
         before("prepare suite", function(done) {
@@ -220,21 +243,26 @@ var Test = {
         tests(accounts);
       };
 
-      global.contract = function(name, tests) {
-        Mocha.describe("Contract: " + name, function() { template.bind(this, tests)(); });
+      global.contract = (name, tests) => {
+        Mocha.describe(`Contract: ${name}`, function() {
+          template.bind(this, tests)();
+        });
       };
 
-      global.contract.only = function(name, tests){
-        Mocha.describe.only("Contract: " + name, function() { template.bind(this, tests)(); });
+      global.contract.only = (name, tests) => {
+        Mocha.describe.only(`Contract: ${name}`, function() {
+          template.bind(this, tests)();
+        });
       };
 
-      global.contract.skip = function(name, tests){
-        Mocha.describe.skip("Contract: " + name, function() { template.bind(this, tests)(); });
+      global.contract.skip = (name, tests) => {
+        Mocha.describe.skip(`Contract: ${name}`, function() {
+          template.bind(this, tests)();
+        });
       };
 
       accept();
-    });
-  }
+    })
 };
 
 module.exports = Test;
